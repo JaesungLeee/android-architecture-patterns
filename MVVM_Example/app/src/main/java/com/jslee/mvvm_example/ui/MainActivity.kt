af1252d9bc89d8e34d9e0e79ce27a5dd1a2af3ef
Jaesung Lee <viki0114@naver.com>
2022-01-05 17:59:48 +0900

    [style] 프로젝트 디렉토리 구조 수정

--- MVVM_Example/app/src/main/java/com/jslee/mvvm_example/ui/MainActivity.kt
+++ MVVM_Example/app/src/main/java/com/jslee/mvvm_example/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jslee.mvvm_example
+package com.jslee.mvvm_example.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jslee.mvvm_example.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
